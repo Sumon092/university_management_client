36f6e1f38fad9a93e2a3727960860f21714b605a
Sumon <m.sumon@outlook.com>
2023-09-28 00:11:17 +0600

    role based side bar done

---
+++ src/app/(withLayout)/super-admin/page.tsx
@@ -0,0 +1,10 @@
+
+const SuperAdminPage = () => {
+    return (
+        <div>
+            <h2>This page is for super admin</h2>
+        </div>
+    );
+};
+
+export default SuperAdminPage;
--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -1,51 +1,41 @@
 'use client';
 import { Menu,Layout } from "antd";
-import type { MenuProps } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-type MenuItem = Required<MenuProps>['items'][number];
-import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-  } from '@ant-design/icons';
+const {  Sider } = Layout;
+
 import {useState}from 'react'
+import { sidebarItems } from "@/constants/sidebarItems";
+import { USER_ROLE } from "@/constants/role";
 
-function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-  ): MenuItem {
-    return {
-      key,
-      icon,
-      children,
-      label,
-    } as MenuItem;
-  }
-  
-  const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-  ];
-
-const SideBar = () => {
+  const SideBar = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const role=USER_ROLE.STUDENT
     return (
-        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
+        <Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}
+            width={230}
+            style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'sticky',
+                left: 0,
+                top: 0,
+                bottom: 0,
+            }}
+        >
+            <div style={{
+                color:'white',
+                fontSize:'1.5rem',
+                textAlign:'center',
+                fontWeight:'bold',
+                marginBottom:'1rem'
+            }}>PH-University</div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={sidebarItems(role)} />
+        </Sider>
     );
 };
 
 export default SideBar;
+
+
