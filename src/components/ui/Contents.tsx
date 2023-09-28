aeb994bfabf97aef9a067cbfca8edad6c45f834c
Sumon <m.sumon@outlook.com>
2023-09-28 11:25:41 +0600

    bredcrumb added

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,9 +1,23 @@
 "use client";
 import { Layout } from "antd";
+import UMBreadCrumb from "./UMBredCrumb";
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
-  return <Content style={{ minHeight: "100vh" }}>{children}</Content>;
+  const base='admin'
+  return <Content style={{ minHeight: "100vh" }}>
+    <UMBreadCrumb items={[
+      {
+        label:`${base}`,
+        link:`/${base}`
+      },
+      {
+        label:`student`,
+        link:`/${base}/student`
+      },
+    ]}></UMBreadCrumb>
+    {children}
+    </Content>;
 };
 
 export default Contents;
