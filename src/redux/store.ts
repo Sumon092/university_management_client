a979f0ffb3c3b678f0b0b9b08ed02c7c85165e41
Sumon <m.sumon@outlook.com>
2023-09-28 16:05:14 +0600

    redux setup done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
+import { authApi } from "./api/authApi";
 import { reducer } from "./rootReducer";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
